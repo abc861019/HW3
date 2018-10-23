f5b1a7d29ebab58ab49e9c2693688d3ad472ce20
Oscar <qazwer57977515@gmail.com>
2018-10-23 23:44:01 +0800

    Project init

---
+++ test_p15/src/test_p15/test_p15.java
@@ -0,0 +1,35 @@
+package test_p15;
+
+public class test_p15 {
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new Car();
+		
+		int number = 1234;
+		double gasoline = 20.5;
+		
+		car1.setNumGas(number,gasoline);
+	}
+
+}
+
+class Car
+{
+	int num;
+	double gas;
+	
+	void setNumGas(int n, double g)
+	{
+		num = n;
+		gas = g;
+		System.out.println("將車號設為" +num +",油量是" +gas); 
+	}
+	void show()
+	{
+		System.out.println("車號是" +num);
+		System.out.println("油量是" +gas);
+		
+	}
+	
+}
---
+++ test_p9/src/test_p9/test_p9.java
@@ -0,0 +1,27 @@
+package test_p9;
+
+public class test_p9 {
+
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new Car();
+		
+		car1.num = 1234;
+		car1.gas = 20.5;
+	}
+}
+
+class Car
+{
+	
+	
+	int num ;
+	double gas;
+	
+	void show()
+	{
+		System.out.println("car1的車號" +this.num);
+		System.out.println("car1的油量" +this.gas);
+	}
+}
